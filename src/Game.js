bc3781822fefabc5e9e289876610e771c93c459a
Rki0 <pky00823@gmail.com>
2022-11-30 11:24:17 +0900

    feat(BridgeMaker): 무작위 값으로 다리 생성하는 메서드 구현
    다리 길이만큼 0 혹은 1 을 랜덤하게 생성한다.
    0일 경우 아래 다리를 의미하는 'D'가
    1일 경우 위 다리를 의미하는 'U'가 canWalkBridge 배열에 push된다.

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,8 @@
 const OutputView = require('./view/OutputView');
 const InputView = require('./view/InputView');
 const SizeValidation = require('./validation/SizeValidation');
+const BridgeMaker = require('./BridgeMaker');
+const generator = require('./BridgeRandomNumberGenerator').generate;
 
 class Game {
   constructor() {
@@ -13,12 +15,18 @@ class Game {
 
   handleSize(sizeInput) {
     try {
-      const sizeValidation = new SizeValidation(sizeInput);
-      sizeValidation.checkError();
+      this.validateSize(sizeInput);
+      const size = Number(sizeInput);
+      const canWalkBridge = BridgeMaker.makeBridge(size, generator);
     } catch (err) {
       OutputView.printError(err.message);
       InputView.readBridgeSize(this.handleSize.bind(this));
     }
+  }
+
+  validateSize(sizeInput) {
+    const sizeValidation = new SizeValidation(sizeInput);
+    sizeValidation.checkError();
   }
 }
 
