bae3f518e313320bcff02f4ee09aa8c3064e4779
Rki0 <pky00823@gmail.com>
2022-11-30 10:35:58 +0900

    feat(InputView): 다리 길이 입력 메서드 구현
    게임 시작 직후, 플레이어가 다리 길이를 설정하도록 입력 함수를 실행한다.

---
+++ src/view/InputView.js
@@ -0,0 +1,30 @@
+// readLine은 여기서만 사용 가능
+// 파일 경로 변경 가능
+// 메서드의 인자 변경 가능
+// 메서드 추가 가능
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE } = require('../constant/Constants');
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+const InputView = {
+  /**
+   * 다리의 길이를 입력받는다.
+   */
+  readBridgeSize() {
+    Console.readLine(MESSAGE.ASK_BRIDGE_LENGTH, (sizeInput) => {});
+  },
+
+  /**
+   * 사용자가 이동할 칸을 입력받는다.
+   */
+  readMoving() {},
+
+  /**
+   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   */
+  readGameCommand() {},
+};
+
+module.exports = InputView;
