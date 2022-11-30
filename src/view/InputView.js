3def2930721942a57a0dcc10d0ca9ea45119d3c0
Rki0 <pky00823@gmail.com>
2022-11-30 11:25:48 +0900

    feat(InputView): 이동할 칸 입력 메서드 구현
    플레이어가 이동하고자하는 칸을 입력하는 메서드를 구현함.

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -21,7 +21,9 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(MESSAGE.ASK_WHERE_WANT_TO_GO, (wantGo) => {});
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
