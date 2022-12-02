e77144bdb17c90d7809e7c6b838a26814dea6fa8
Rki0 <pky00823@gmail.com>
2022-12-02 23:03:18 +0900

    fix(MovingValidation): 논리 연산자 수정
    조건문 내에 포함되어있는 조건들을 모두 만족했을 때 예외 처리가 되어야하므로
    && 연산자를 사용하는 것으로 변경함.

--- src/validation/MovingValidation.js
+++ src/validation/MovingValidation.js
@@ -5,7 +5,7 @@ const MovingValidation = {
   validateMoving(moving) {
     if (
       moving !== GAME.LOWER_BRIDGE_STRING
-      || moving !== GAME.UPPER_BRIDGE_STRING
+      && moving !== GAME.UPPER_BRIDGE_STRING
     ) {
       throw new MovingError(ERROR.NOT_VALID_MOVING);
     }
