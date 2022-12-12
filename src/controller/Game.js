f48aded1b63c83d5cb2b45e0da0be8ec5a7aea0f
Rki0 <pky00823@gmail.com>
2022-12-12 10:41:24 +0900

    feat(InputView): 다리 길이 입력 메서드 구현
    다리 길이를 입력 받기 위한 메서드를 구현함.

--- src/controller/Game.js
+++ src/controller/Game.js
@@ -1,9 +1,16 @@
 const OutputView = require('../view/OutputView');
+const InputView = require('../view/InputView');
 
 class Game {
   constructor() {
     OutputView.printStart();
   }
+
+  start() {
+    InputView.readBridgeSize(this.handleBridgeSize);
+  }
+
+  handleBridgeSize = (sizeInput) => {};
 }
 
 module.exports = Game;
