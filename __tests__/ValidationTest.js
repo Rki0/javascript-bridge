b138e7e1bdc8ad846db026beaad539d3d80415fa
Rki0 <pky00823@gmail.com>
2022-12-12 12:21:57 +0900

    test(CommandValidation): 게임 커맨드 입력값 검증 테스트
    게임 커맨드는 재시작을 의미하는 R, 종료를 의미하는 Q 만 입력할 수 있다.
    그 외의 값은 전부 예외처리한다.

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,5 +1,6 @@
 const SizeValidation = require('../src/validation/SizeValidation');
 const MovingValidation = require('../src/validation/MovingValidation');
+const CommandValidation = require('../src/validation/CommandValidation');
 
 describe('검증 메서드 테스트', () => {
   test.each(['5 ', '03', '2', '21'])(
@@ -14,6 +15,12 @@ describe('검증 메서드 테스트', () => {
   test('이동할 칸 입력값 검증 테스트', () => {
     expect(() => {
       MovingValidation.validateMoving('A');
+    }).toThrow();
+  });
+
+  test('커맨드 입력값 검증 테스트', () => {
+    expect(() => {
+      CommandValidation.validateCommand('A');
     }).toThrow();
   });
 });
